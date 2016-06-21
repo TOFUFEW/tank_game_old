a1180f6495da3689f51052d8f6d9df7b44cae806
Kostiantyn Koval <kkoval@sfu.ca>
2016-06-21 16:38:32 -0700

    Add Fortress and Map classes back to records

---
+++ src/Assignment2/core/Fortress.java
@@ -0,0 +1,26 @@
+package Assignment2.core;
+
+/**
+ * Created by constantin on 18/06/16.
+ */
+public class Fortress {
+    //Some comment for the fortress
+    public static final int DEFAULT_HEALTH = 1500;
+    private int health;
+
+    public Fortress(){
+        this.health = DEFAULT_HEALTH;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void getDamage(int damage){
+        this.health -= damage;
+    }
+
+    public boolean isRuined(){
+        return health <= 0;
+    }
+}
