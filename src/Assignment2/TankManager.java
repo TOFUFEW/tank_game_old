2eb5b69964eebada659870e0efbc46a829751b79
Kostiantyn Koval <kkoval@sfu.ca>
2016-06-21 12:36:17 -0700

    Add tankManager, implement attack square

--- src/Assignment2/TankManager.java
+++ src/Assignment2/TankManager.java
@@ -6,5 +6,64 @@ import java.util.List;
  * Created by constantin on 21/06/16.
  */
 public class TankManager {
+    private Map map;
+    private List<Tank> tetromino;
 
+    public TankManager(int dimensionRow, int dimensionCol, int numTanks){
+        map = new Map(dimensionRow, dimensionCol);
+        generateTetrominos(numTanks);
+    }
+
+    public boolean revealTank(Coordinate coordinate) {
+        Square square = map.getSquare(coordinate);
+        if (square.attack()) {
+            //Cell is opened now and there is a tank
+            damageTank(coordinate);
+            map.setSquare(coordinate, square);
+            return true;
+        }
+        return false;
+    }
+
+    public int getTotalDamage(){
+        int total = 0;
+        for(Tank tank : tetromino){
+            total += tank.getDamage();
+        }
+        return total;
+    }
+
+    //----------------------------
+    // ----- Private methods -----
+    //----------------------------
+
+    private void generateTetrominos(int numTanks){
+        //Initialise all tanks
+    }
+
+    private void setTank(Coordinate coordinate) {
+        Square square = new Square();
+        square.set_tank();
+        map.setSquare(coordinate, square);
+    }
+
+    private void finishInitialisation(int dimensionRow, int dimensionCol){
+        for(int i = 0; i < dimensionRow; i++){
+            for(int j = 0; j < dimensionCol; j++){
+                Coordinate coordinate = new Coordinate(i,j);
+                if(map.getSquare(coordinate) == null) {
+                    map.setSquare(coordinate, new Square());
+                }
+            }
+        }
+    }
+
+    private void damageTank(Coordinate coordinate){
+        for(Tank tank : tetromino){
+            if (tank.isUnderAttack(coordinate)){
+                //Tank's health was decreased
+                return;
+            }
+        }
+    }
 }
