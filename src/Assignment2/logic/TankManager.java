a1a339e7f75f1fa8046cd75ba64fdbef2628d000
Kostiantyn Koval <kkoval@sfu.ca>
2016-06-21 13:25:08 -0700

    Implement tank generation

--- src/Assignment2/logic/TankManager.java
+++ src/Assignment2/logic/TankManager.java
@@ -1,11 +1,13 @@
 package Assignment2.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import Assignment2.core.Coordinate;
 import Assignment2.core.Square;
 import Assignment2.core.Tank;
-import Assignment2.core.Coordinate;
 import Assignment2.core.Map;
-import java.util.List;
 
 
 /**
@@ -15,9 +17,9 @@ public class TankManager {
     private Map map;
     private List<Tank> tetromino;
 
-    public TankManager(int dimensionRow, int dimensionCol, int numTanks){
+    public TankManager(int dimensionRow, int dimensionCol, int numTanks, int sizeTanks){
         map = new Map(dimensionRow, dimensionCol);
-        generateTetrominos(numTanks);
+        generateTetrominos(dimensionRow, dimensionCol, numTanks, sizeTanks);
     }
 
     public boolean revealTank(Coordinate coordinate) {
@@ -43,11 +45,48 @@ public class TankManager {
     // ----- Private methods -----
     //----------------------------
 
-    private void generateTetrominos(int numTanks){
-        //Initialise all tanks
+    private void generateTetrominos(int dimensionRow, int dimensionCol, int numTanks, int sizeTanks) {
+        for(int i = 0; i < numTanks; i++){
+            createTank(dimensionRow, dimensionCol, sizeTanks);
+        }
     }
 
-    private void setTank(Coordinate coordinate) {
+    private void createTank(int dimensionRow, int dimensionCol, int sizeTanks){
+        Random random = new Random();
+        Coordinate nextSquare;
+        boolean emptySquare;
+
+        do {
+            int row = random.nextInt(dimensionRow);
+            int col = random.nextInt(dimensionCol);
+
+            nextSquare = new Coordinate(row, col);
+            emptySquare = map.getSquare(nextSquare).getIfEmpty();
+        } while (!emptySquare);
+
+        List<Coordinate> tankPositions = new ArrayList<>();
+        tankPositions.set(0, nextSquare);
+
+        for (int i = 1; i < sizeTanks; i++) {
+            List<Coordinate> possiblePositions = new ArrayList<>();
+            possiblePositions.addAll(searchForAvailableSpot(nextSquare)); //TODO Add positions that didnt repeat!!!
+
+            int index = random.nextInt(possiblePositions.size());
+            nextSquare = possiblePositions.get(index);
+            
+            setOccupied(nextSquare);
+            tankPositions.set(i, nextSquare);
+        }
+
+        Tank newTank = new Tank(tankPositions);
+        tetromino.add(newTank);
+    }
+
+    private List<Coordinate> searchForAvailableSpot(Coordinate coordinate){
+        return new ArrayList<>(); //TODO
+    }
+
+    private void setOccupied(Coordinate coordinate) {
         Square square = new Square();
         square.set_tank();
         map.setSquare(coordinate, square);
